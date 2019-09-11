65e3d642d069471a7ec724c4c8235c805fcfe43c
Pawel Piatek <pawelpiatek98@gmail.com>
2019-09-11 20:39:14 +0200

    basic style for main nav icons

--- src/components/SideNavigation/SideNavigation.js
+++ src/components/SideNavigation/SideNavigation.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCoffee,faFax} from "@fortawesome/free-solid-svg-icons";
+import {faTasks,faClipboardList,faCodeBranch } from "@fortawesome/free-solid-svg-icons";
 
 const Nav = styled.nav`
 height: 100vh;
@@ -14,11 +14,15 @@ padding: 0 1rem;
 const MainNavItems = styled.ul`
   list-style: none;
   text-align: left;
-  box-shadow: 0px 7px 8px -9px rgba(0,0,0,0.75);
+  box-shadow: 0px 7px 8px -10px rgba(0,0,0,0.75);
   padding: 3rem;
   
     li{
     margin: 1rem 0;
+      
+      p{
+      display: inline-block;
+      }
     }
 `;
 
@@ -28,9 +32,9 @@ const sideNavigation = ()=>{
     return(
         <Nav>
             <MainNavItems>
-                <li> <FontAwesomeIcon icon={faFax}/></li>
-                <li>Projects</li>
-                <li>Technologies</li>
+                <li> <FontAwesomeIcon icon={faTasks}/> <p>Tasks</p></li>
+                <li> <FontAwesomeIcon icon={faClipboardList}/> <p>Projects</p></li>
+                <li> <FontAwesomeIcon icon={faCodeBranch}/> <p>Technologies</p></li>
             </MainNavItems>
         </Nav>
     )
