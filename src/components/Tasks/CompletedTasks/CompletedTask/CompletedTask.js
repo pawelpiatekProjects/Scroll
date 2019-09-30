0cff564f0a239cd26ec3be63eab77f05e4287508
Pawel Piatek <pawelpiatek98@gmail.com>
2019-09-30 11:51:42 +0200

    improved completed task

--- src/components/Tasks/CompletedTasks/CompletedTask/CompletedTask.js
+++ src/components/Tasks/CompletedTasks/CompletedTask/CompletedTask.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faHeart,faCheck,faInfo} from "@fortawesome/free-solid-svg-icons";
-
+import * as variables from '../../../../utils/variables';
 
 
 const Task = styled.div`
@@ -14,8 +14,8 @@ align-content: center;
 justify-items: center;
 align-items: center;
 
-width: 80%;
-background: #FCFCFC;
+width: 100%;
+background: ${variables.primaryGrey};
 margin: 2rem auto;
 padding: 1rem 3rem 3rem 3rem;
 position: relative;
@@ -25,15 +25,17 @@ border-radius: .5rem;
 const Title = styled.p`
 grid-column: 1/span 3;
 grid-row: 1/ span 1;
-font-size: 1.2rem;
+font-size: 1.8rem;
 font-weight: bold;
 text-align: left;
+color: ${variables.fontColorFirst};
 `;
 
 const Deadline = styled.div`
 grid-column: 4/span 3;
 grid-row: 1/ span 1;
-font-size: 1.2rem;
+font-size: 1.8rem;
+color: ${variables.fontColorFirst};
 span{
 font-weight: bold;
 }
@@ -53,17 +55,18 @@ const Control = styled.button`
 
 
   padding: .5rem;
-  width: 3rem;
+  width: 4rem;
+  font-size: 1.8rem;
   border: none;
   border-radius: 2rem;
-  color: #fff;;
+  color: ${variables.white};
   background: ${props=>props.color};
   margin: 0 .25rem;
   
   
   &:hover{
   cursor: pointer;
-  background: #fff;
+  background: ${variables.white};
   border: 1px solid ${props=>props.color};
   color: ${props=>props.color};
   }
@@ -75,7 +78,9 @@ const Content = styled.p`
 grid-column: 1/ -1;
 grid-row: 2/span 1;
 text-align: left;
-border-top: 2px solid ${props=>props.important ?'#FF6C5F': '#8EC06C'} ;
+font-size: 1.6rem;
+color: ${variables.fontColorFirst};
+border-top: 2px solid ${props=>props.important ?`${variables.primaryRed}`: `${variables.primaryGreen}`} ;
 padding: 2rem 0;
 width: 100%;
 `;
@@ -89,7 +94,7 @@ const completedTask = (props)=>{
             <Title>{props.title}</Title>
             <Deadline><span>Completed Task</span></Deadline>
             <Controlls>
-                <Control color='#FFCC2F'>
+                <Control color={variables.primaryYellow}>
                     <FontAwesomeIcon icon={faInfo}/>
                 </Control>
             </Controlls>
