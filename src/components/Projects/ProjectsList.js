aeb9b676226bde41b8e732f896628dea416fb0aa
Pawel Piatek <pawelpiatek98@gmail.com>
2019-10-08 20:55:53 +0200

    added projectsList component

--- src/components/Projects/ProjectsList.js
+++ src/components/Projects/ProjectsList.js
@@ -1,1 +1,19 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const ProjectsListWrapper = styled.div`
+padding: 6rem;
+display: grid;
+grid-template-columns: repeat(2,1fr);
+grid-gap: 6rem;
+`;
+
+const projectsList = (props)=>{
+    return(
+        <ProjectsListWrapper>
+            Projects
+        </ProjectsListWrapper>
+    )
+};
+
+export default projectsList;
